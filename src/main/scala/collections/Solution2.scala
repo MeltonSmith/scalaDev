58028964ef2afa17c3bfd436e02e1f1a9823c02a
Melton <ravenglc1@gmail.com>
2023-08-07 02:20:20 +0500

    Some solutions

--- src/main/scala/collections/Solution2.scala
+++ src/main/scala/collections/Solution2.scala
@@ -6,12 +6,19 @@ import scala.io.StdIn
 object Solution2 {
 
   def main(args: Array[String]): Unit = {
-    def kOrder(list: List[Int], k: Int): Int = {
-      val list: List[Int] = StdIn.readLine().split(" ").map(_.toInt).toList
-      val head = list.sorted.drop(k - 1).head
-      println(head)
-      head
-    }
+
+    val k = StdIn.readInt()
+    val list: List[Int] = StdIn.readLine().split(" ").map(_.toInt).toList
+    val head = list.sorted.drop(k).head
+    println(head)
+    head
+
+//    def kOrder(list: List[Int], k: Int): Int = {
+//      val list: List[Int] = StdIn.readLine().split(" ").map(_.toInt).toList
+//      val head = list.sorted.drop(k - 1).head
+//      println(head)
+//      head
+//    }
 
 
   }
