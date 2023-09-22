1b74ef55c598a2d43813369d99df993e9c701532
Yan_Rakhmatullin <YRakhmatullin@datatech.ru>
2023-09-22 13:08:05 +0500

    fixed mem in two sums scala

--- src/main/scala/algo/yandex/easy/twosum/Solution.scala
+++ src/main/scala/algo/yandex/easy/twosum/Solution.scala
@@ -1,7 +1,6 @@
 package r.ian
 package algo.yandex.easy.twosum
 
-import scala.collection.mutable.ArrayBuffer
 import scala.util.control.Breaks.{break, breakable}
 
 /**
@@ -12,21 +11,19 @@ object Solution {
   def twoSum(nums: Array[Int], target: Int): Array[Int] = {
     val map = scala.collection.mutable.Map[Int, Int]()
 
-    val result = ArrayBuffer[Int]()
+    var result:Array[Int] = null
 
     breakable{
       for (i <- nums.indices) {
-        map.get(nums(i)) match {
+        map.get(target - nums(i)) match {
           case Some(value) =>
-            result.+=(value, i)
+            result = Array(value, i)
             break
           case _ =>
         }
-        map += ((target - nums(i), i))
+        map += ((nums(i), i))
       }
     }
-
-
-    result.toArray
+    result
   }
 }
