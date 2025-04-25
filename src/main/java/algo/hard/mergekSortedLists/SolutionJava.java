726753cb935ae0be8ef300b2f1100c0bfa19489e
MeltonSmith <ravenglc1@gmail.com>
2025-04-25 15:15:57 +0500

    k sorted java serial optimization

--- src/main/java/algo/hard/mergekSortedLists/SolutionJava.java
+++ src/main/java/algo/hard/mergekSortedLists/SolutionJava.java
@@ -1,6 +1,5 @@
 package r.ian.algo.hard.mergekSortedLists;
 
-import java.util.Arrays;
 
 /**
  * @author Melton Smith
@@ -8,28 +7,31 @@ import java.util.Arrays;
  */
 public class SolutionJava {
 
-    private static final int treshhold = 2;
 
-    public static ListNode mergeKLists(ListNode[] lists) {
+    public ListNode mergeKLists(ListNode[] lists) {
         if (lists == null || lists.length == 0) {
             return null;
         }
-        else if (lists.length < treshhold){
+        else if (lists.length < 2){
             return lists[0];
         }
-        else {
-            int i = lists.length / treshhold;
-
-            //divide
-            var a1 = mergeKLists(Arrays.stream(lists, 0, i).toArray(ListNode[]::new));
-            var a2 = mergeKLists(Arrays.stream(lists, i, lists.length).toArray(ListNode[]::new));
-
-            //conquer
-            return serialSort(a1, a2);
-        }
+        return divide(lists, 0, lists.length-1);
     }
 
-    private static ListNode serialSort(ListNode a1, ListNode a2) {
+    public ListNode divide(ListNode[] lists, int l, int r) {
+        if (l == r) return lists[l];
+        int mid = (l + r) / 2;
+        //divide
+        var a1 = divide(lists, l, mid);
+        var a2 = divide(lists, mid + 1, r);
+
+        //conquer
+        return serialSort(a1, a2);
+
+
+    }
+
+    private  ListNode serialSort(ListNode a1, ListNode a2) {
         if (a1 == null && a2 == null) {
             return null;
         }
