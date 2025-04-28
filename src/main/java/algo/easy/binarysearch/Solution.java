eafb1d0931a3f9fefaee2bbad6c768c10d5fe146
MeltonSmith <ravenglc1@gmail.com>
2025-04-28 23:48:36 +0500

    easy binary search

---
+++ src/main/java/algo/easy/binarysearch/Solution.java
@@ -0,0 +1,28 @@
+package r.ian.algo.easy.binarysearch;
+
+/**
+ * @author Melton Smith
+ * @since 28.04.2025
+ */
+public class Solution {
+    public int search(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[mid] < target) {
+                left = mid + 1;
+            }
+            if (nums[mid] > target) {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
