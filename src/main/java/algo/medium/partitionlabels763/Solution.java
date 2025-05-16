e4243e616d1061343ad3d7523663fffb146e4b45
MeltonSmith <ravenglc1@gmail.com>
2025-05-17 03:09:04 +0500

    763 partition labels

---
+++ src/main/java/algo/medium/partitionlabels763/Solution.java
@@ -0,0 +1,53 @@
+package r.ian.algo.medium.partitionlabels763;
+
+import scala.Char;
+
+import java.util.*;
+
+/**
+ * @author Melton Smith
+ * @since 17.05.2025
+ */
+public class Solution {
+    public List<Integer> partitionLabels(String s) {
+        int[] charsAscii = new int[127];
+        //todo
+        Arrays.fill(charsAscii, -1);
+        int[] partitions = new int[s.length() + 1];
+        int lastActivePartitionIndex = -1;
+        int newPartitionStartIndex = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int c = s.charAt(i);
+            if (charsAscii[c] == -1) {
+                newPartitionStartIndex = i;
+                lastActivePartitionIndex++;
+                partitions[lastActivePartitionIndex] = newPartitionStartIndex;
+            }
+            else { //seen
+                while (charsAscii[c] < newPartitionStartIndex) { //seen in previous partition, "merge" partitions
+                    partitions[lastActivePartitionIndex] = 0;
+                    lastActivePartitionIndex--;
+                    newPartitionStartIndex = partitions[lastActivePartitionIndex];
+                }
+            }
+            charsAscii[c] = i;
+        }
+
+        LinkedList<Integer> res = new LinkedList<>();
+        for (int i = 0; i < partitions.length; i++) {
+            if (partitions[i] != 0 && (i == partitions.length - 1 || partitions[i+1] == 0)) {
+                res.add(s.length() - partitions[i]);
+                break;
+            }
+            else if (partitions[i] == 0 && partitions[i+1] == 0) {
+                res.add(s.length() - partitions[i]);
+                break;
+            }
+            else
+                res.add(partitions[i+1] - partitions[i]);
+
+        }
+
+        return res;
+    }
+}
---
+++ src/test/java/algo/medium/partitionlabels763/SolutionTest.java
@@ -0,0 +1,21 @@
+package r.ian.algo.medium.partitionlabels763;
+
+import org.junit.Test;
+
+import java.util.List;
+
+/**
+ * @author Melton Smith
+ * @since 17.05.2025
+ */
+public class SolutionTest {
+
+    @Test
+    public void firstTest() {
+        Solution solution = new Solution();
+//        List<Integer> ababcbacadefegdehijhklij = solution.partitionLabels("ababcbacadefegdehijhklij");
+//        List<Integer> ababcbacadefegdehijhklij = solution.partitionLabels("eccbbbbdec");
+        List<Integer> ababcbacadefegdehijhklij = solution.partitionLabels("caedbdedda");
+        System.out.println(ababcbacadefegdehijhklij);
+    }
+}
