94a294b86bdaa252c6009055332c736cfdf52060
MeltonSmith <ravenglc1@gmail.com>
2025-05-19 02:06:37 +0500

    Same tree ez

---
+++ src/main/java/algo/easy/sametree/TreeNode.java
@@ -0,0 +1,24 @@
+package r.ian.algo.easy.sametree;
+
+/**
+ * @author Melton Smith
+ * @since 19.05.2025
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
