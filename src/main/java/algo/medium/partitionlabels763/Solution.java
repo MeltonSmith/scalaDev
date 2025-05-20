cdf91909a613a1af7089a65a090e93510a411880
MeltonSmith <ravenglc1@gmail.com>
2025-05-20 19:51:11 +0500

    labels

--- src/main/java/algo/medium/partitionlabels763/Solution.java
+++ src/main/java/algo/medium/partitionlabels763/Solution.java
@@ -17,6 +17,7 @@ public class Solution {
         int newPartitionStartIndex = 0;
         for (int i = 0; i < s.length(); i++) {
             int c = s.charAt(i);
+            //not seen, starting a new partition
             if (charsAscii[c] == -1) {
                 newPartitionStartIndex = i;
                 lastActivePartitionIndex++;
