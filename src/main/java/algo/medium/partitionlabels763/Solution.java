90568cd2ea9b6f4483e8fe25cb736f404cfca957
MeltonSmith <ravenglc1@gmail.com>
2025-05-20 21:53:13 +0500

    labels with array list

--- src/main/java/algo/medium/partitionlabels763/Solution.java
+++ src/main/java/algo/medium/partitionlabels763/Solution.java
@@ -50,6 +50,48 @@ public class Solution {
         return res;
     }
 
+    //92% 19%
+    public List<Integer> partitionLabelsWitArrayList(String s) {
+        int[] charsAscii = new int[26];
+        Arrays.fill(charsAscii, -1);
+        int a = 'a';
+        List<Integer> partitions = new ArrayList<>();
+        int lastActivePartitionIndex = -1;
+        int newPartitionStartIndex = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int c = s.charAt(i);
+            //not seen, starting a new partition
+            if (charsAscii[c-a] == -1) {
+                newPartitionStartIndex = i;
+                lastActivePartitionIndex++;
+                partitions.add(lastActivePartitionIndex, newPartitionStartIndex);
+            } else { //seen
+                while (charsAscii[c-a] < newPartitionStartIndex) { //seen in previous partition, moving back and "merge" partitions
+                    partitions.set(lastActivePartitionIndex, 0);
+                    lastActivePartitionIndex--;
+                    newPartitionStartIndex = partitions.get(lastActivePartitionIndex);
+                }
+            }
+            charsAscii[c-a] = i;
+        }
+
+        if (lastActivePartitionIndex == -1) {
+            partitions.add(s.length());
+            return partitions;
+        }
+
+        for (int i = 0; i < partitions.size(); i++) {
+            if (i == lastActivePartitionIndex) {
+                partitions.set(i, s.length() - partitions.get(i));
+                break;
+            } else
+                partitions.set(i, partitions.get(i + 1) - partitions.get(i));
+        }
+
+        return partitions.subList(0, lastActivePartitionIndex+1);
+    }
+
+
     //97% 42%
     public List<Integer> partitionLabelsWitjLinkedList(String s) {
         int[] charsAscii = new int[26];
