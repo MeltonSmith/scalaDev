c0e9acd9eab31bb34c87fca6f003feeee88a0e0d
MeltonSmith <ravenglc1@gmail.com>
2025-07-29 17:49:14 +0500

    13 with enums

--- src/main/java/algo/easy/_13/Solution.java
+++ src/main/java/algo/easy/_13/Solution.java
@@ -9,16 +9,48 @@ import java.util.Map;
  */
 public class Solution {
 
-    private List<Character> chars = List.of('I', 'V', 'X', 'L', 'C', 'D', 'M');
-    private Map<Character, Integer> map = Map.of('I', 1, 'V', 5, 'X', 10, 'L', 50, 'C', 100, 'D', 500, 'M', 1000);
+    enum RomanSign {
+
+        I ('I', 1),
+        V('V',5),
+        X('X',10),
+        L('L',50),
+        C('C',100),
+        D('D',500),
+        M('M',1000);
+
+        final char sign;
+        final int decimalValue;
+
+        RomanSign(char sign, int decimalValue) {
+            this.sign = sign;
+            this.decimalValue = decimalValue;
+        }
+
+    }
+
+    public RomanSign defineEnum(char n) {
+        return switch (n) {
+            case 'I' -> RomanSign.I;
+            case 'V' -> RomanSign.V;
+            case 'X' -> RomanSign.X;
+            case 'L' -> RomanSign.L;
+            case 'C' -> RomanSign.C;
+            case 'D' -> RomanSign.D;
+            case 'M' -> RomanSign.M;
+            default -> null;
+        };
+    }
+
+
 
     public int romanToInt(String s) {
         char[] input = s.toCharArray();
-        int res = map.get(input[0]);
+        int res =  defineEnum(input[0]).decimalValue;
         for (int i = 1; i < input.length; i++) {
-            res += map.get(input[i]);
-            if (chars.indexOf(input[i-1]) < chars.indexOf(input[i])) {
-                res -= 2* map.get(input[i-1]);
+            res += defineEnum(input[i]).decimalValue;
+            if (defineEnum(input[i-1]).compareTo(defineEnum(input[i])) < 0 ) {
+                res -= 2* defineEnum(input[i-1]).decimalValue;
             }
 
         }
