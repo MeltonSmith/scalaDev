8ed0f508af5090ce3ef5f8c9e8b0fa931990339e
MeltonSmith <ravenglc1@gmail.com>
2025-08-10 12:35:03 +0500

    26

---
+++ src/main/java/algo/easy/_26/Solution.java
@@ -0,0 +1,23 @@
+package r.ian.algo.easy._26;
+
+/**
+ * @author Melton Smith
+ * @since 10.08.2025
+ */
+public class Solution {
+    //59% 91%
+    public int removeDuplicates(int[] nums) {
+        int indexToSet = 1;
+        int prevValue = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != prevValue) {
+                if (i != indexToSet) {
+                    nums[indexToSet] = nums[i];
+                }
+                prevValue = nums[i];
+                indexToSet++;
+            }
+        }
+        return indexToSet;
+    }
+}
